218ab766f42d6bb7dea63ae94483f054258fe319
pierregibrenne <pierre.gibrenne@protonmail.com>
2024-07-12 18:34:11 +0200

    remove

--- src/client.js
+++ src/client.js
@@ -2,5 +2,5 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://autheqstmjgeindgkrse.supabase.co'
-const supabaseKey = "NEXT_PUBLIC_SUPABASE_ANON_KEY"
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF1dGhlcXN0bWpnZWluZGdrcnNlIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTA3MjY0MTUsImV4cCI6MjAwNjMwMjQxNX0.1bERfsM-wKgtTBhaC5Z2vRIfob_DEMgWyA6vgmxKCow"
 export const supabase = createClient(supabaseUrl, supabaseKey)
