896de30788b9bf2707e06d0c0a1c52c2e8729041
pierregibrenne <pierre.gibrenne@protonmail.com>
2024-07-12 18:39:13 +0200

    .env

--- src/client.js
+++ src/client.js
@@ -2,5 +2,5 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://autheqstmjgeindgkrse.supabase.co'
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF1dGhlcXN0bWpnZWluZGdrcnNlIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTA3MjY0MTUsImV4cCI6MjAwNjMwMjQxNX0.1bERfsM-wKgtTBhaC5Z2vRIfob_DEMgWyA6vgmxKCow"
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY
 export const supabase = createClient(supabaseUrl, supabaseKey)
